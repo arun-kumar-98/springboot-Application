94a33a849863d765a296b48cca12580ca691ffa0
unknown <arunrits15@gmail.com>
2022-02-03 18:12:47 +0530

    main class modified

--- src/main/java/com/demo/SpringbootApplicationTestingApplication.java
+++ src/main/java/com/demo/SpringbootApplicationTestingApplication.java
@@ -2,12 +2,17 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootApplicationTestingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplicationTestingApplication.class, args);
+		ApplicationContext ac = SpringApplication.run(SpringbootApplicationTestingApplication.class, args);
+	
+		((ConfigurableApplicationContext) ac).close();
+	
 	}
 
 }
